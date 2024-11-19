241ae5033dd4285ef125c21021fd95b20d4dff90
Mitaka <mitaka001962@gmail.com>
2024-11-19 20:35:59 +0900

    update modal width

--- src/components/side-search/search-result-card-for-create.tsx
+++ src/components/side-search/search-result-card-for-create.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import { Track } from "@/lib/definitions";
 import SearchResultCard from "./search-result-card";
 import CoverImage from "@/components/search/cover-image";
 import MetadataInfo from "@/components/search/metadata-info";
 import PreviewButton from "@/components/search/preview-button";
+import { useRef } from "react";
 
 export default function SearchResultCardForCreate({
   result,
@@ -11,19 +14,16 @@ export default function SearchResultCardForCreate({
   result: Track;
   handleAdd: (track: Track) => void;
 }) {
-  const clickHandler = () => {
-    const elm = document.getElementById(`create_card_modal_${result.id}`) as HTMLDialogElement | null;
-    elm?.showModal();
-  };
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
   return (
     <>
-      <button className="text-left" onClick={clickHandler}>
+      <button className="text-left" onClick={() => dialogRef.current?.showModal()}>
         <SearchResultCard result={result} />
       </button>
       {/* --modal-- */}
-      <dialog id={`create_card_modal_${result.id}`} className="modal">
-        <div className="modal-box max-w-[80%] w-fit">
+      <dialog ref={dialogRef} className="modal">
+        <div className="modal-box min-w-[50%] max-w-[80%] w-fit">
           <h3 className="font-bold text-2xl px-2 pb-2 mb-4 border-b border-base-content/15">楽曲情報</h3>
           <div className="flex gap-4 px-2">
             <div className="flex-none w-1/3 max-w-[260px] flex flex-col gap-4">
